8b452c4c9931086d32b9909e430c86dc4086254a
iperevoschikov <iperevoschikov@skbkontur.ru>
2023-08-10 10:21:18 +0500

    add projects from template

---
+++ Vostok.CodeAnalysis.StructuredLogging.Test/VostokCodeAnalysisStructuredLoggingUnitTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = Vostok.CodeAnalysis.StructuredLogging.Test.Verifiers.CSharpCodeFixVerifier<
+    Vostok.CodeAnalysis.StructuredLogging.VostokCodeAnalysisStructuredLoggingAnalyzer,
+    Vostok.CodeAnalysis.StructuredLogging.VostokCodeAnalysisStructuredLoggingCodeFixProvider>;
+
+namespace Vostok.CodeAnalysis.StructuredLogging.Test;
+
+[TestClass]
+public class VostokCodeAnalysisStructuredLoggingUnitTest
+{
+    //No diagnostics expected to show up
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        var test = @"";
+
+        await VerifyCS.VerifyAnalyzerAsync(test);
+    }
+
+    //Diagnostic and CodeFix both triggered and checked for
+    [TestMethod]
+    public async Task TestMethod2()
+    {
+        var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+        var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+        var expected = VerifyCS.Diagnostic("VostokCodeAnalysisStructuredLogging").WithLocation(0).WithArguments("TypeName");
+        await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+    }
+}
